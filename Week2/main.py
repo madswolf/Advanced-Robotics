a4a432805c73b276e7ef924a0cc34a166a8dcf1c
julianbrandt <julianbrrandt@gmail.com>
2022-09-14 14:25:34 +0200

    upped speed and generalized turn speeds

--- Week2/main.py
+++ Week2/main.py
@@ -16,7 +16,8 @@ from random import choice
 # Click "Open user guide" on the EV3 extension tab for more information.
 
 sensor_threshold = 10
-robot_speed = 60 # mm/s
+robot_speed = 100 # mm/s
+time_factor = 60 / robot_speed # 60 = magic number that everything is calibrated with
 turn_speed = 90 # deg/s
 sensor_state = [False, False]
 
@@ -33,42 +34,34 @@ angle = robot.state()[2]
 ev3.speaker.set_volume(20)
 
 def intersection_move(direction):
+    clamped_time_factor = max(time_factor, 0.6)
     if direction == "straight":
-        robot.stop()
-        ev3.speaker.beep(2000)
         robot.drive(robot_speed, 0)
-        wait(1500)
+        wait(1500 * time_factor)
     elif direction == "right":
-        robot.stop()
-        ev3.speaker.beep(3000)
-        robot.drive(90, 90)
-        wait(1000)
+        robot.drive(90 / clamped_time_factor, 90 / clamped_time_factor)
+        wait(1000 * clamped_time_factor)
         robot.drive(robot_speed, 0)
-        wait(1000)
+        wait(1000 * clamped_time_factor)
     elif direction == "left":
-        robot.stop()
-        ev3.speaker.beep(1000)
-        robot.drive(50, -90)
-        wait(1000)
+        robot.drive(50 / clamped_time_factor, -90 / clamped_time_factor)
+        wait(1000 * clamped_time_factor)
         robot.drive(robot_speed, 0)
-        wait(1000)
+        wait(1000 * clamped_time_factor)
     elif direction == "180":
-        robot.stop()
-        ev3.speaker.beep(4000)
         robot.drive(70, 180)
         wait(1000)
         robot.drive(robot_speed, 0)
-        wait(1000)
+        wait(1000 * time_factor)
     elif direction == "backwards":
-        robot.stop()
         robot.drive(-robot_speed, 0)
-        wait(2000)
+        wait(2000 * time_factor)
         robot.drive(70, 180)
         wait(1000)
     elif direction == "stop":
         sys.exit(0)
 
-move_sequence = ["right", "left", "straight", "backwards", "stop"]
+move_sequence = ["straight", "left"]
 move_pointer = 0
 
 # Write your program here.
@@ -80,7 +73,7 @@ while True:
 
     if new_state != sensor_state:
         sensor_state = new_state
-        robot.drive(robot_speed, (detectedM*2-1) * turn_speed*0.2)
+        robot.drive(robot_speed, (detectedM*2-1) * turn_speed*0.2 / time_factor)
     elif not (detectedL and detectedR):
         intersection_move(move_sequence[move_pointer])
         angle = robot.state()[2]
