2eeb9921b0bde974ff5cee800233a7ea294191b2
julianbrandt <julianbrrandt@gmail.com>
2022-11-09 15:15:05 +0100

    added ambient data to sensor analysis

--- Exam/ground_sensor_analysis.py
+++ Exam/ground_sensor_analysis.py
@@ -1,17 +1,21 @@
 import sys
 import statistics
 
-lines = [x.split(",") for x in sys.stdin]
-sens_l = [int(x[0]) for x in lines]
-sens_r = [int(x[1]) for x in lines]
+reflected, ambient = list(zip(*[x.split() for x in sys.stdin]))
+ref_l, ref_r = list(zip(*[map(int, x.split(",")) for x in reflected]))
+amb_l, amb_r = list(zip(*[map(int, x.split(",")) for x in ambient]))
 
-mean_l = statistics.mean(sens_l)
-mean_r = statistics.mean(sens_r)
+mean_ref_l = statistics.mean(ref_l)
+mean_ref_r = statistics.mean(ref_r)
+mean_amb_l = statistics.mean(amb_l)
+mean_amb_r = statistics.mean(amb_r)
 
-stdev_l = statistics.stdev(sens_l)
-stdev_r = statistics.stdev(sens_r)
+stdev_ref_l = statistics.stdev(ref_l)
+stdev_ref_r = statistics.stdev(ref_r)
+stdev_amb_l = statistics.stdev(amb_l)
+stdev_amb_r = statistics.stdev(amb_r)
 
-print(f"left mean:                {mean_l}")
-print(f"right mean:               {mean_r}")
-print(f"left standard deviation:  {stdev_l}")
-print(f"right standard deviation: {stdev_r}")
+print(f"reflected mean:                ({mean_ref_l}, {mean_ref_r})")
+print(f"reflected standard deviation:  ({stdev_ref_l}, {stdev_ref_r})")
+print(f"ambient mean:                  ({mean_amb_l}, {mean_amb_r})")
+print(f"ambient standard deviation:    ({stdev_amb_l}, {stdev_amb_r})")
